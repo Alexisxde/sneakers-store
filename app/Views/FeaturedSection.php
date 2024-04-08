5bbc05cfc3c16096b52e37aa9688f5f850351499
Alexisxde <olivarezalexis749@gmail.com>
2024-04-08 17:59:19 -0300

    styles: set the page to 1920px so that it looks "good" and fix some styles 💄

--- app/Views/FeaturedSection.php
+++ app/Views/FeaturedSection.php
@@ -6,95 +6,40 @@
     <span>featured</span>
   </div>
   <div class="row">
-    <div class="card col-2 col-lg-4">
-      <img src="./assets/img/snakers/1.webp">
-      <div class="card-body">
-        <h5 class="text-center">Nike Air Force 1</h5>
-        <div class="text-center pb-1">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
+
+    <?php
+    $ruta_json = APPPATH . 'Json/products.json';
+    $jsonData = file_get_contents($ruta_json);
+    $products = json_decode($jsonData, true);
+
+    if (count($products["featured"]) != 0) {
+      foreach ($products["featured"] as $featured) {
+    ?>
+        <div class="card col-2 col-lg-4">
+          <img src="<?= $featured["img"] ?>">
+          <div class="card-body">
+            <h5 class="text-center"><?= $featured["title"] ?></h5>
+            <div class="text-center pb-1">
+              <?php
+              $stars = $featured["stars"];
+              $wholeStars = floor($stars);
+              $halfStar = ($stars - $wholeStars) >= 0.5;
+
+              for ($i = 0; $i < $wholeStars; $i++) echo '<i class="bi bi-star-fill"></i> ';
+              if ($halfStar) echo '<i class="bi bi-star-half"></i> ';
+              for ($i = 0; $i < (5 - $wholeStars - ($halfStar ? 1 : 0)); $i++) echo '<i class="bi bi-star"></i> ';
+              ?>
+            </div>
+            <div class="prices text-center">
+              <span class="prev-price">$ <?= $featured["price"] ?></span>
+              <span>$ <?= number_format($featured["price"] * (1 - $featured["discount"] / 100), 3); ?></span>
+              <div class="text-success"><?= $featured["discount"] ?>% de descuento</div>
+            </div>
+          </div>
         </div>
-        <div class="price text-center">
-          <span>$ 269.999</span>
-          <span>$ 161.999</span>
-          <span class="text-success">40% de descuento</span>
-        </div>
-      </div>
-    </div>
-    <div class="card col-2 col-lg-4">
-      <img src="./assets/img/snakers/2.webp">
-      <div class="card-body">
-        <h5 class="text-center">Zion 3</h5>
-        <div class="text-center pb-1">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-half"></i>
-          <i class="bi bi-star"></i>
-        </div>
-        <div class="price text-center">
-          <span>$ 259.999</span>
-          <span>$ 194.999</span>
-          <span class="text-success">25% de descuento</span>
-        </div>
-      </div>
-    </div>
-    <div class="card col-2 col-lg-4">
-      <img src="./assets/img/snakers/3.webp">
-      <div class="card-body">
-        <h5 class="text-center">Nike Revolution 6</h5>
-        <div class="text-center pb-1">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-half"></i>
-          <i class="bi bi-star"></i>
-        </div>
-        <div class="price text-center">
-          <span>$ 275.999</span>
-          <span>$ 206.999</span>
-          <span class="text-success">25% de descuento</span>
-        </div>
-      </div>
-    </div>
-    <div class="card col-2 col-lg-4">
-      <img src="./assets/img/snakers/4.webp">
-      <div class="card-body">
-        <h5 class="text-center">Nike Winflo 10</h5>
-        <div class="text-center pb-1">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star"></i>
-        </div>
-        <div class="price text-center">
-          <span>$ 179.999</span>
-          <span>$ 143.999</span>
-          <span class="text-success">20% de descuento</span>
-        </div>
-      </div>
-    </div>
-    <div class="card col-2 col-lg-4">
-      <img src="./assets/img/snakers/5.webp">
-      <div class="card-body">
-        <h5 class="text-center">Air Jordan 1 Low</h5>
-        <div class="text-center pb-1">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-half"></i>
-        </div>
-        <div class="price text-center">
-          <span>$ 209.999</span>
-          <span>$ 188.999</span>
-          <span class="text-success">10% de descuento</span>
-        </div>
-      </div>
-    </div>
+    <?php
+      }
+    }
+    ?>
   </div>
 </section>
--- app/Views/GalerySection.php
+++ app/Views/GalerySection.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="/assets/styles/GalerySection.component.css">
 
-<section class="container-fluid galery-section">
+<section class="galery-section container-fluid">
   <div class="galery-title">
     <h3>Top Trending Styles</h3>
     <p>Here to bring your life style yo the next level.</p>
