20b1882039691432c72d9cae6f167d9a39ec8b34
Alexisxde <olivarezalexis749@gmail.com>
2024-04-16 16:55:23 -0300

    new terms to terms & conditions page ✨

--- app/Views/pages/TermsConditions.php
+++ app/Views/pages/TermsConditions.php
@@ -15,7 +15,6 @@
     <p>Estos Términos describen las reglas aplicables al uso de un sitio web, experiencia digital, una plataforma de medios sociales, una aplicación móvil, una tecnología portátil de propiedad de SNEAKERS o uno de nuestros otros productos o servicios, todos los cuales forman parte de la Plataforma.</p>
     <p>Usted puede acceder a la Plataforma a través de una computadora, teléfono móvil, tableta, consola u otra tecnología similar ("Dispositivo"). Las tarifas y gastos de su empresa de telecomunicaciones se aplican a su Dispositivo y están a su cargo.</p>
     <p>Estos Términos constituyen un acuerdo legalmente vinculante entre usted ("Usted" o "Usuario"), Para contactarnos usted dispone de nuestros canales de comunicación, que detallamos <a href='<?= base_url('contacts') ?>'>aquí</a>. Al hacer clic en el casillero de aceptación, Usted declara haber leído detenidamente, comprendido y consentido estos Términos.</p>
-
     <div>
       <p>A continuación, listamos algunos puntos importantes para Usted:</p>
       <ul>
@@ -30,14 +29,13 @@
         </li>
       </ul>
     </div>
-
     <div class='rules'>
-      <span>1- REGLAS BÁSICAS</span>
+      <span>REGLAS BÁSICAS</span>
       <p>Requisitos. Para poder usar la Plataforma Usted deberá haber cumplido dieciocho (18) años, no estar declarado judicialmente como incapaz, o tener la capacidad restringida para contratar o disponer de su patrimonio o contar con el consentimiento de su progenitores, tutor o representante legal. Aunque es responsabilidad del usuario cumplir con este requisito. El objetivo de ese pedido siempre estará relacionado con la protección de la masa de usuarios.</p>
       <p>EN LA MÁXIMA MEDIDA PERMITIDA POR LA LEY APLICABLE, SNEAKERS NO ES RESPONSABLE DE NINGUNA PÉRDIDA O ACTIVIDAD QUE SURJA DEL USO NO AUTORIZADO DE SU INFORMACIÓN PERSONAL.</p>
     </div>
     <div class='rules'>
-      <span>2- CÓDIGO DE CONDUCTA DEL USUARIO</span>
+      <span>CÓDIGO DE CONDUCTA DEL USUARIO</span>
       <div>
         <p>Nos emociona su contribución a la comunidad SNEAKERS. Estas son algunas reglas básicas:</p>
         <ul>
@@ -58,22 +56,41 @@
       </div>
     </div>
     <div class='rules'>
-      <span>3- TERCEROS EN LA PLATAFORMA</span>
+      <span>TERCEROS EN LA PLATAFORMA</span>
       <p>Cuando lo considere oportuno, SNEAKERS puede vincularse o asociarse con sitios web de terceros, plataformas de redes sociales, aplicaciones móviles y otros productos y servicios. Puede conectarse con estos Terceros a través de la Plataforma, pero esto no significa que SNEAKERS avale, monitoree o tenga control sobre estos Terceros o sus actividades, que están sujetos a términos de uso y políticas de privacidad independientes. Debe revisar cuidadosamente los sitios de terceros, así como sus términos de uso y política de privacidad. SNEAKERS no es responsable del contenido, las políticas o las actividades de Terceros, y Usted decide interactuar con Terceros bajo su propio riesgo.</p>
     </div>
-
     <div class='rules'>
-      <span>4- RESCISIÓN</span>
-
+      <span>RESCISIÓN</span>
       <p>De acuerdo a la legislación aplicable, Nosotros podremos rescindir o modificar la Plataforma, los productos o servicios en cualquier momento, sin previo aviso, por iniciativa propia o por petición de terceros.
         De acuerdo a la legislación aplicable, Nosotros podremos restringir el uso que Usted hace de la Plataforma o cualquier parte de ésta cuando identifiquemos que Usted realizó un uso indebido o sospechoso de la Plataforma, sin generar ninguna responsabilidad imputable a SNEAKERS.</p>
       <ul>
         <li>Estos Términos permanecerán vigentes incluso después de que haya dejado de usar la Plataforma.</li>
       </ul>
     </div>
-    <div class="rules">
-      <span>5- ATENCIÓN AL CONSUMIDOR/USUARIO</span>
+    <div class='rules'>
+      <span>ATENCIÓN AL CONSUMIDOR/USUARIO</span>
       <p>Al utilizar nuestra plataforma, Usted podrá utilizar el chat de Whatsapp que le proporcionaremos, con el fin de que se pueda contactar con nosotros para manifestarnos sus consultas, entendiéndose el mismo como un canal de atención al cliente. Dicho chat va a contar con un número telefónico y un redireccionamiento directo desde la Plataforma hasta el mismo. La finalidad del chat de referencia será exclusivamente despejar las inquietudes de los usuarios, no efectuar o concretar algún tipo de venta.</p>
+    </div>
+    <div class='rules'>
+      <span>MONEDA Y FORMAS DE PAGO.</span>
+      <p>Todos los precios de este sitio web están expresados en pesos argentinos, moneda de curso legal de la República Argentina. Todos los medios de pago están sujetos a que el importe sea debidamente acreditado y/o verificado.</p>
+      <p>Contamos con un sistema de cobro llamado Mercado Pago, Este acepta todas las tarjetas de crédito y efectivo a través de pago fácil y rapipago. La empresa SNEAKER no se responsabiliza por las ofertas o descuentos ofrecidos por Mercado Pago.</p>
+      <p>Contamos con el medio de pago "Tarjetas de crédito", nuestro sistema acepta todas aquellas tarjetas pertenecientes a una entidad bancaria. Si usted cuenta con algun problema para realizar el pago por este medio deberá contactarese a sneakersstore@gmail.com y allí un asesor le guiará.</p>
+    </div>
+    <div class='rules'>
+      <span>INSTRUCCIONES PARA DEVOLUCIÓN DE PRODUCTO POR CAMBIO</span>
+      <p>Ud. deberá comunicarse con nuestro centro de atención al cliente de SNEAKERS: <b>cambios@sneakersstore.com.ar</b> para que registre y autorice su solicitud de devolución, indicando el/los productos que quiere realizar el cambio,el motivo del mismo y por que producto y talle desea cambiarlo. Los productos adquiridos deberán ser devueltos en perfectas condiciones, sin uso, con su embalaje original y el ticket de compra. Usted dispone de 30 días corridos a partir del día de la fecha de recepción del producto para gestionar el cambio, comunicarse con el centro de atención al cliente (<b>cambios@sneakersstore.com.ar</b>) e indicar el código del articulo del producto que desea adquirir por cambio, junto a la medida correspondiente. El costo del envío es gratis siempre y cuando el valor de la compra supere los $215.000, de lo contrario correrá por cuenta y orden del cliente. Una vez enviado el/los productos de cambio es necesario enviar por mail al representante de SNEAKERS con quien se ha comunicado anteriormente para enviarle el Código de Seguimiento del mismo. Los cambios están sujetos a disponibilidad de stock; la empresa no se responsabiliza por variación en el precio del nuevo producto seleccionado. En el envió del producto de cambio, usted deberá adoptar las medidas de precaución necesarias al embalar el paquete. SNEAKERS se reserva el derecho de rechazar cualquier devolución de producto que no esté bajo el proceso del proveedor logístico asignado.</p>
+    </div>
+    <div class='rules'>
+      <span>TIEMPOS DE ENTREGA</span>
+      <p>SNEAKERS realizará la entrega en la dirección que usted ingresó dentro del territorio argentino ingresado sólo puede realizar la entrega en una dirección de entrega que sea una dirección del domicilio o de oficina para los códigos postales disponibles. Las entregas se realizarán en días hábiles. Todas las entregas irán acompañadas de un acuse de recibo.</p>
+      <p>Los envíos se realizan por medio Correo Argentino. El envió normal será entregado el producto dentro de los 10 días hábiles.</p>
+      <p>Los tiempos de entrega son indicativos y, por consiguiente, no se consideran fechas límite estrictas. El mero hecho de haber excedido un periodo de entrega no le dará ningún derecho de compensación.</p>
+      <p>Al concretar la operación, el usuario se está obligando a revisar la mercadería al momento de recibirla en el domicilio que éste consigne. La firma del remito correspondiente implicará la aceptación de la mercadería adquirida, sin perjuicio de los derechos que al usuario le asisten conforme la ley.</p>
+    </div>
+    <div class='rules'>
+      <span>ACEPTACIÓN Y CONOCIMIENTO DE LOS TÉRMINOS Y CONDICIONES></span>
+      <p>Los presentes términos y condiciones tienen carácter obligatorio y vinculante. Se aplican a todas las comprar y actividades realizadas mediante nuestro sitio web. El uso de esta implica conocimiento y aceptación, deberá abstenerse de utilizar nuestro sitio web y/o los servicios por el ofrecidos.</p>
     </div>
     <p class='contact'>Si tienes alguna pregunta sobre estos términos y condiciones, <a href='<?= base_url('contacts') ?>'>Contactanos</a>.</p>
 </main>
