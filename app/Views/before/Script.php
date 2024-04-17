255d300f89d5df5507cd83109c8fc64f785b07e5
Alexisxde <olivarezalexis749@gmail.com>
2024-04-17 20:57:57 -0300

    Deleted the folder from the files that was used for the headers 🔥

--- app/Views/before/Script.php
+++
@@ -1,9 +0,0 @@
-<script>
-  document.getElementById("logo").addEventListener("click", function() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-</script>
-<script src="/assets/js/bootstrap.min.js" integrity="" crossorigin=""></script>
