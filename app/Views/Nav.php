516cccd1929018fb84321f72482113c60ae563c8
Alexisxde <olivarezalexis749@gmail.com>
2024-04-19 18:41:06 -0300

    I fixed the logo to take you back to the beginning 🐛

--- app/Views/Nav.php
+++ app/Views/Nav.php
@@ -2,7 +2,7 @@
   <nav class='navbar navbar-expand-lg bg-white p-2'>
     <div class='container-fluid'>
       <div class='fw-bold text-center fs-4 m-2'>
-        <a class='logo' href='/'>SNEAKERS</a>
+        <a class='logo' href='<?= base_url() ?>'>SNEAKERS</a>
       </div>
       <button class='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent'>
         <i class='bi bi-list text-black fs-1'></i>
