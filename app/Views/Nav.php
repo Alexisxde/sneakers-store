a18165fa79acb3bb09c6ab0b40b2b1422e18663a
Alexisxde <olivarezalexis749@gmail.com>
2024-05-29 19:32:14 -0300

    Nav styles 💄

--- app/Views/Nav.php
+++ app/Views/Nav.php
@@ -4,31 +4,24 @@
       <div class='fw-bold text-center fs-4 m-2'>
         <a class='logo' href='<?= base_url() ?>'>SNEAKERS</a>
       </div>
-      <button class='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent'>
-        <i class='bi bi-list text-black fs-1'></i>
-      </button>
       <div class='collapse navbar-collapse' id='navbarSupportedContent'>
         <div class='ms-auto'>
           <ul class='navbar-nav text-center'>
+            <li class='nav-item'>
+              <a class='nav-link' href='<?= base_url('about-us') ?>'>NOSOTROS</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='<?= base_url('contacts') ?>'>CONTACTO</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='<?= base_url('commercialization') ?>'>COMERCIALIZACIÓN</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='<?= base_url('sneakers') ?>'>SNEAKERS</a>
+            </li>
             <?php if (isset(session()->username) && session()->rol === 'admin') : ?>
               <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('sneakers') ?>'>VER SNEAKERS</a>
-              </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('users') ?>'>VER USUARIOS</a>
-              </li>
-            <?php else : ?>
-              <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('about-us') ?>'>NOSOTROS</a>
-              </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('contacts') ?>'>CONTACTO</a>
-              </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('commercialization') ?>'>COMERCIALIZACIÓN</a>
-              </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='<?= base_url('sneakers') ?>'>SNEAKERS</a>
+                <a class='nav-link' href='<?= base_url('users') ?>'>USUARIOS</a>
               </li>
             <?php endif; ?>
           </ul>
@@ -42,6 +35,9 @@
           <?php endif; ?>
         </div>
       </div>
+      <button class='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent'>
+        <i class='bi bi-list text-black fs-1'></i>
+      </button>
     </div>
   </nav>
 </header>
